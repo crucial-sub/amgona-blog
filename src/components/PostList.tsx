64229844e93e17294047f3fe7a2f0ae5eba08201
crucial-sub <jssub940@gmail.com>
2023-02-19 20:39:38 +0900

    Dev 페이지 스타일링 적용

---
+++ src/components/PostList.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import React from 'react'
+import { PostDetailType } from 'utils/types'
+
+interface PostListProps {
+  post: PostDetailType
+  title: string
+}
+
+const PostList = ({ post, title }: PostListProps) => {
+  const { slug, frontmatter } = post
+
+  return (
+    <article>
+      <Link className={'desktop:postList'} href={`/post/${slug}`}>
+        <div
+          className={
+            'flex desktop:items-center desktop:flex-row mobile:flex-col'
+          }
+        >
+          <div
+            className={
+              'relative desktop:w-48 desktop:h-48 mobile:w-full desktop:mb-0 desktop:mr-12 mobile:mb-5 shrink-0 overflow-hidden drop-shadow-md rounded-xl'
+            }
+          >
+            <img
+              className={
+                'desktop:w-48 desktop:h-48 desktop:aspect-square aspect-[700:400] mobile:w-full mobile:h-[205px] tablet:h-auto object-cover thumbnail'
+              }
+              src={frontmatter.thumbnail}
+              alt="thumbnail"
+            />
+            <div className={'absolute top-0 left-0 w-full h-full overlay'} />
+          </div>
+
+          <div>
+            <h3
+              className={
+                'max-h-16 mobile:mb-[10px] desktop:mb-4 desktop:text-3xl mobile:text-2xl font-bold text-ellipsis break-keep'
+              }
+            >
+              {frontmatter.title}
+            </h3>
+            <p className={'max-h-24 mb-3 text-base text-gray'}>
+              {frontmatter.excerpt}
+            </p>
+            <span className={'text-sm text-lightGray'}>{frontmatter.date}</span>
+          </div>
+        </div>
+      </Link>
+    </article>
+  )
+}
+
+export default PostList
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,20 +1,29 @@
-import Link from 'next/link'
-import React from 'react'
+import { memo, useEffect, useState } from 'react'
 import { PostListType } from 'utils/types'
+import PostList from './PostList'
 
 const Posts = ({ title, posts }: PostListType) => {
   return (
-    <div>
-      <div>{title}</div>
-      <div>
+    <div
+      className={
+        'flex flex-col justify-center desktop:max-w-[980px] mobile:w-screen mobile:px-6 desktop:mb-24 mobile:mb-16 desktop:m-auto'
+      }
+    >
+      <section
+        className={'flex flex-col desktop:my-16 mobile:mt-10 mobile:mb-8'}
+      >
+        <h2 className={'desktop:mb-12 mobile:mb-6 text-3xl font-bold'}>
+          {title}
+        </h2>
+      </section>
+
+      <section className={'flex flex-col gap-14 mt-2'}>
         {posts.map(post => (
-          <Link href={`post/${post.slug}`} key={post.slug}>
-            <div>{post.slug}</div>
-          </Link>
+          <PostList key={post.slug} post={post} title={title} />
         ))}
-      </div>
+      </section>
     </div>
   )
 }
 
-export default Posts
+export default memo(Posts)
